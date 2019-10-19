f23141e19670a221dd927e4f72c8f158f7d3b8d7
vijaykanthveeraiyan <v.vijaykanth5@gmail.com>
2019-10-19 23:46:24 +0530

    Update ListItem.js

--- component/ListItem.js
+++ component/ListItem.js
@@ -1,20 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-function ListItems(props) {
-  let tdItems = [];
-  tdItems = Object.keys(props.data).map((key, idx) => (
-    <td onClick={props.itemClick.bind(this, props.data[key])}>
-      {props.data[key] ? props.data[key] : "___"}
-    </td>
-  ));
-
+function ListItem(props) {
   return (
-    <tr data-idx={props.idx} key={props.idx}>
-      <th className="sticky-col">{props.idx + 1}</th>
-      {tdItems}
-    </tr>
-  );
+    Object.keys(props.data).map((key, idx) => (
+        <td onClick={props.itemClick.bind(this, props.data[key])}>
+          {props.data[key] ? props.data[key] : "___"}
+        </td>
+      ))
+  )
 }
 
-export default ListItems;
+export default ListItem;
